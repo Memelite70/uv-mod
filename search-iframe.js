89aee52a701e872862b3efebd2dc6bf59aa3eef1
Geometry Tryhard <memelite69@gmail.com>
2024-08-26 12:00:16 -0400

    update

--- search-iframe.js
+++ search-iframe.js
@@ -34,7 +34,7 @@ function getRandomUpTo(max) {
  return Math.floor(Math.random() * Math.floor(max)) + 1;
 }
   const url = search(address.value, searchEngine.value);
-  var iframe = document.createElementById('iframe');
+  var iframe = document.createElement('iframe');
   iframe.style = 'position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;';
   iframe.src = '//dont-sue-me-topvaz.topvazgeo.online/launch.html?domain=' + url;
 });
