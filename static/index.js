221ca63a929b0d69123c50abbfcb11350b409282
Geometry Tryhard <memelite69@gmail.com>
2024-08-26 13:32:45 -0400

    update

--- static/index.js
+++ static/index.js
@@ -34,5 +34,27 @@ function getRandomUpTo(max) {
  return Math.floor(Math.random() * Math.floor(max)) + 1;
 }
   const url = search(address.value, searchEngine.value);
-  location.href = '//dont-sue-me-topvaz.topvazgeo.online/launch.html?domain=' + url;
+  var alert = localStorage.getItem('alert')
+  if (alert === 'true'){
+  }
+  else {
+    alert('To make the proxy function, right-click the screen and select reload/refresh frame. You should only need to do this once.');
+    localStorage.setItem('alert', 'true')
+  }
+  location.href = '//' + localStorage.getItem('server') + '.topvazgeo.online/launch.html?domain=' + url;
+  if (localStorage.getItem('server' === 'undefined'){
+    localStorage.setItem('server', 'dont-sue-me-topvaz');
+  }
+      
+  
 });
+function getRandomUpTo(max) {
+ return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
+function changeServer(){
+  var input = getElementById('input');
+  localStorage.setItem('server'); = input.src;
+}
+function changeServer(random){
+  localStorage.setItem('server', getRandomUpTo(10000000000000));
+  )
