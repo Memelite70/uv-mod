b9a83909834e9092f66e637bb82894540818eaff
Geometry Tryhard <memelite69@gmail.com>
2024-08-26 10:48:07 -0400

    update

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'https://dont-sue-me-topvaz.topvazgeo.online/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
+    handler: 'https://dont-sue-me-topvaz.topvazgeo.online/uv/uv.handler.js',
+    bundle: 'https://dont-sue-me-topvaz.topvazgeo.online/uv/uv.bundle.js',
     config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
+    sw: 'https://dont-sue-me-topvaz.topvazgeo.online/uv/uv.sw.js',
 };
