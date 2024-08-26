f540dc1ac585d2e37c83806d67a5a4afe6e7acc6
Geometry Tryhard <memelite69@gmail.com>
2024-08-26 11:00:19 -0400

    update

--- static/register-sw.js
+++ static/register-sw.js
@@ -25,6 +25,6 @@ async function registerSW() {
 
   // Ultraviolet has a stock `sw.js` script.
   await navigator.serviceWorker.register(stockSW, {
-    scope: 'https://dont-sue-me-topvaz.topvazgeo.online/school/',
+    scope: __uv$config.prefix,
   });
 }
